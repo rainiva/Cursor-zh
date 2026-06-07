e59035b8b16a5f541c22d50877df3d4a9b84c32f
rainiva <coxtean@foxmail.com>
2026-06-07 18:13:41 +0800

    fix(lib): wire default mapping loader to data.js

---
+++ scripts/lib/mapping/data.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+function resolveDefaultsDir() {
+  return path.join(__dirname, '..', '..', '..', 'translations', 'overlay', 'defaults');
+}
+
+function readDefaultMappings(fileName) {
+  const filePath = path.join(resolveDefaultsDir(), fileName);
+  if (!fs.existsSync(filePath)) {
+    return [];
+  }
+  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+}
+
+function defaultCursorWinCommonMappings() {
+  return readDefaultMappings('cursor-win.common.json');
+}
+
+function defaultCursorWinDynamicMappings() {
+  return readDefaultMappings('cursor-win.dynamic.json');
+}
+
+function defaultOverlayMappings() {
+  return readDefaultMappings('workbench.overlay.json');
+}
+
+module.exports = {
+  defaultCursorWinCommonMappings,
+  defaultCursorWinDynamicMappings,
+  defaultOverlayMappings,
+};
